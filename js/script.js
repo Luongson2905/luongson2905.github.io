08bedda28e01b4599956ace8d32f4317caa74ce7
kens3dola <58432333+kens3dola@users.noreply.github.com>
2024-01-25 21:30:44 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".wapper");
 const btn = document.getElementById("spin");
 const result = document.querySelector(".result");
-const gifts = ["one", "two", "three", "four", "five", "Đèn", "Ấm điện", "TV"];
+const gifts = ["one", "two", "three", "four", "ABC", "Đèn", "Ấm điện", "TV"];
 const correct = [1, 2, 3, 4, 5, 6];
 const SEGMENT = 45;
 let pause = false;
